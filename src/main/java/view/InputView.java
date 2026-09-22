86272712bd6b135383c8f426a334456946a5bef8
손서진 <sonseojin702@gmail.com>
2026-09-22 20:50:01 +0900

    feat: InputView 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+  Scanner scanner = new Scanner(System.in);
+
+  public String getPurchasePrice() {
+    System.out.println("구입금액을 입력해 주세요.");
+    String purchasePrice = scanner.nextLine();
+    return purchasePrice;
+  }
+}
