87505b90765f9fccd35a5cefd1c764db502f1aa5
손서진 <sonseojin702@gmail.com>
2026-09-23 21:31:20 +0900

    refactor: 출력 형식에 맞게 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 import domain.Lotto;
 import domain.Lottos;
+import dto.ResultDto;
 import java.util.List;
 
 public class OutputView {
@@ -16,5 +17,15 @@ public class OutputView {
 
   private void printLotto(Lotto lotto) {
     System.out.println(lotto);
+  }
+
+  public void printResult(ResultDto resultDto) {
+    System.out.println("당첨 통계");
+    System.out.println("---------");
+    System.out.println("3개 일치 (5000원)- " + resultDto.winning3() + "개");
+    System.out.println("4개 일치 (50000원)- " + resultDto.winning4() + "개");
+    System.out.println("5개 일치 (1500000원)- " + resultDto.winning5() + "개");
+    System.out.println("6개 일치 (2000000000원)- " + resultDto.winning6() + "개");
+    System.out.println("총 수익률은 " + resultDto.rateOfReturn() + "입니다.");
   }
 }
