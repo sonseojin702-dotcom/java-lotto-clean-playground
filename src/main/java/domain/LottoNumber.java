3fa38a9efa6548af8b6aae12032a2b05ebed7f77
손서진 <sonseojin702@gmail.com>
2026-09-23 16:13:14 +0900

    refactor: 오버라이드 equals, hashCode 추가

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -15,6 +15,23 @@ public class LottoNumber implements Comparable<LottoNumber> {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LottoNumber)) {
+      return false;
+    }
+    LottoNumber other = (LottoNumber) o;
+    return value == other.value;
+  }
+
+  @Override
+  public int hashCode() {
+    return Integer.hashCode(value);
+  }
+
+  @Override
   public int compareTo(LottoNumber other){
     return Integer.compare(this.value, other.value);
   }
