875c1ebd26d71445b1fd75579ed33e1ade7414d0
손서진 <sonseojin702@gmail.com>
2026-09-26 05:02:22 +0900

    test: 사용되지 않은 Import 문 제거

--- src/test/java/domain/winning/LottoRankTest.java
+++ src/test/java/domain/winning/LottoRankTest.java
@@ -2,9 +2,7 @@ package domain.winning;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.EnumMap;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
