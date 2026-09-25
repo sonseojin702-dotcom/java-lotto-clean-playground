dbe5c72a5f6873406fed2ed4e662fe145d60a651
손서진 <sonseojin702@gmail.com>
2026-09-26 02:44:37 +0900

    test: LottoTest 생성

---
+++ src/test/java/domain/purchase/LottoTest.java
@@ -0,0 +1,40 @@
+package domain.purchase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTest {
+    @Test
+    @DisplayName("당첨 번호와 0개 일치하면 일치 개수는 0이다.")
+    void returnZeroWhenZeroNumberMatch() {
+        Lotto lotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = Lotto.from(List.of(7, 8, 9, 10, 11, 12));
+
+        int count = lotto.getMatchCount(winningLotto);
+        assertEquals(0, count);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 3개 일치하면 일치 개수는 3이다.")
+    void returnThreeWhenThreeNumberMatch() {
+        Lotto lotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = Lotto.from(List.of(1, 3, 5, 7, 9, 11));
+
+        int count = lotto.getMatchCount(winningLotto);
+        assertEquals(3, count);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 6개 일치하면 일치 개수는 6이다.")
+    void returnSixWhenSixNumberMatch() {
+        Lotto lotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.getMatchCount(winningLotto);
+        assertEquals(6, count);
+    }
+}
