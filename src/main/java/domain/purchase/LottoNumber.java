3595b6e195bdce1a8fd4da2d120a363744edb894
손서진 <sonseojin702@gmail.com>
2026-09-25 22:14:53 +0900

    refactor: 로또 범위(1~45) 매직넘버로 표기

--- src/main/java/domain/purchase/LottoNumber.java
+++ src/main/java/domain/purchase/LottoNumber.java
@@ -2,6 +2,8 @@ package domain.purchase;
 
 public class LottoNumber implements Comparable<LottoNumber> {
     private final int value;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER = 1;
 
     public LottoNumber(int value) {
         validate(value);
@@ -9,7 +11,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
     }
 
     private void validate(int value) {
-        if (value < 1 || value > 45) {
+        if (value < MIN_LOTTO_NUMBER || value > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("로또의 범위는 1~45 사이여야 합니다.");
         }
     }
