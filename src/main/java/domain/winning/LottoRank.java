9dd5c03717002b288fe86e31ac2c2c41be14787a
손서진 <sonseojin702@gmail.com>
2026-09-26 03:52:14 +0900

    refactor: 생성자 매개변수 long으로 통일

--- src/main/java/domain/winning/LottoRank.java
+++ src/main/java/domain/winning/LottoRank.java
@@ -7,16 +7,16 @@ import java.util.Arrays;
  * 선언 순서가 출력순서 이므로 3개 일치부터 선언.
  */
 public enum LottoRank {
-    MISS(0, 0),
     THREE(3, 5000),
     FOUR(4, 50000),
     FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    SIX(6, 2000000000),
+    MISS(0, 0);
 
     private final int matchCount;
     private final long prize;
 
-    LottoRank(int matchCount, int prize) {
+    LottoRank(int matchCount, long prize) {
         this.matchCount = matchCount;
         this.prize = prize;
     }
